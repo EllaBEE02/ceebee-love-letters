c103af16a423aa69dc3ae5414e3beb3af39f129e
EllaBEE02 <emmanuellabesiru@gmail.com>
2024-12-29 13:35:33 +0100

    Added images images folder, styled new letters section

--- javascript/script.js
+++ javascript/script.js
@@ -13,6 +13,10 @@ function showEmotion(response) {
   }
 }
 
+let letter2Element = document.querySelector("#letter-2");
+letter2Element.innerHTML = "Please check-in tomorrow for more letters from BEE";
+letter2Element.style.opacity = 0.7;
+
 let heartRateElement = document.querySelector("#heart-rate");
 heartRateElement.addEventListener("change", showEmotion);
 console.log(heartRateElement);
